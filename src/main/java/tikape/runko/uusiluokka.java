7d6658d2dd6f97f5c0069c8e9f5215d472ecf1f5
Arttu <Arttu@172.17.77.226>
2017-02-07 14:35:02 +0200

    Lisäsin luokan 'uusiluokka'

---
+++ src/main/java/tikape/runko/uusiluokka.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tikape.runko;
+
+/**
+ *
+ * @author Arttu
+ */
+public class uusiluokka {
+    private String testi;
+    
+    public uusiluokka(String testi) {
+        this.testi = testi;
+    }
+}
