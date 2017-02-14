ba2062360d379870812fa2f9690bd2d5e3f3eaf8
Olli <oltsuwh@gmail.com>
2017-02-14 13:31:09 +0200

    Muokattiin Viestiketju-luokkaa

--- src/main/java/tikape/runko/domain/Viestiketju.java
+++ src/main/java/tikape/runko/domain/Viestiketju.java
@@ -1,5 +1,43 @@
 package tikape.runko.domain;
 
+import java.sql.Timestamp;
+
 public class Viestiketju {
-    
+
+    private int tunnus;
+    private int alue;
+    private String otsikko;
+    private Timestamp aikaleima;
+
+    public Viestiketju(int tunnus, int alue, String otsikko, Timestamp aikaleima) {
+        this.tunnus = tunnus;
+        this.alue = alue;
+        this.otsikko = otsikko;
+        this.aikaleima = aikaleima;
+    }
+
+    public int getTunnus() {
+        return tunnus;
+    }
+
+    public int getAlue() {
+        return alue;
+    }
+
+    public String getOtsikko() {
+        return otsikko;
+    }
+
+    public Timestamp getAikaleima() {
+        return aikaleima;
+    }
+
+    public void setOtsikko(String otsikko) {
+        this.otsikko = otsikko;
+    }
+
+    public void setAikaleima(Timestamp aikaleima) {
+        this.aikaleima = aikaleima;
+    }
+
 }
